016d39dfe4af94440665e1cff5c8024d3248eb4b
ylevanon <yoavlevanoni@outlook.com>
2024-11-28 16:25:20 -0800

    adding tailwind first

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,9 @@
-// Learn more https://docs.expo.dev/guides/monorepos
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('node:path');
+const { withNativeWind } = require('nativewind/metro');
 
-// Find the project and workspace directories
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
+/** @type {import('expo/metro-config').MetroConfig} */
+// eslint-disable-next-line no-undef
+const config = getDefaultConfig(__dirname);
 
-const config = getDefaultConfig(projectRoot);
-
-// 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
-// 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules')
-];
-// #3 - Force resolving nested modules to the folders below
-config.resolver.disableHierarchicalLookup = true;
-config.resolver.unstable_enableSymlinks = true;
-
-module.exports = config;
+module.exports = withNativeWind(config, { input: './global.css' });
